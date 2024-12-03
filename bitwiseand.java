1b267c272a79c3b966bad89c483e73e3aad7d472
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-03 16:01:13 +0530

    Add files via upload

--- bitwiseand.java
+++ bitwiseand.java
@@ -4,6 +4,6 @@ public class bitwiseand{
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        System.out.println((a+b)-(a&b));
+        System.out.println(a+b-(a|b));
     }
 }
