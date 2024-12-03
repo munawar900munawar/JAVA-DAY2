4816b362313adbae90290ea40427a939faf94bb1
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-03 11:00:49 +0530

    Add files via upload

---
+++ assign.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class assign{
+    public static void main(String args[]){
+        int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9);
+        System.out.println(a*=2);
+    }
+}
