fea00d86629f7eec7fc5b8480c94025a3591e090
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-03 14:11:48 +0530

    Add files via upload

---
+++ swap1.java
@@ -0,0 +1,13 @@
+import.java.util.*;
+public class swap1{
+    public static void main(String args[]){
+        Scanner sc=new scanner(System.in);
+        int b=sc.nextInt();
+        int g=sc.nextInt();
+        int c=b;
+        b=g;
+        g=c;
+        String res=String.format("after swapping \nboy =%d \ngirl =%d",b,g);
+        System.out.println(res);
+    }
+}
