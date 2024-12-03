9adab600950468f88a0c6d03cb2e59e736bb37b8
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-03 10:28:57 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class relational{
+    public static void main(String args[]){
+        int a=8,b=5;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+    }
+}
